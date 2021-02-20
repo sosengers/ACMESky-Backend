c8654d11b98adfc4cad121782d76d3872b0cf838
Marco Ferrati <marco.ferrati@gmail.com>
2021-02-20 17:48:49 +0100

    Change behaviour of ACMESky-Backend moved business logic to camunda-workers

--- acmesky/controllers/default_controller.py
+++ acmesky/controllers/default_controller.py
@@ -8,7 +8,10 @@ from acmesky.models.offer_purchase_data import OfferPurchaseData  # noqa: E501
 from acmesky.models.payment_information import PaymentInformation  # noqa: E501
 from acmesky import util
 
-from pymongo import MongoClient
+import json
+import requests
+
+camunda_base_url = "http://camunda_acmesky:8080/engine-rest"
 
 def buy_offer(offer_purchase_data=None):  # noqa: E501
     """buyOffer
@@ -53,20 +56,25 @@ def register_interest(interest=None):  # noqa: E501
     if connexion.request.is_json:
         interest = Interest.from_dict(connexion.request.get_json())  # noqa: E501
 
-    username = "root"
-    password = "password"
-    client = MongoClient(f"mongodb://{username}:{password}@acmesky_mongo:27017")
-    acmesky_db = client['ACMESky']
-    interests_collection = acmesky_db['interests']
     interest_dict = interest.to_dict()
     interest_dict['min_departure_date'] = interest.min_departure_date.isoformat()
     interest_dict['max_comeback_date'] = interest.max_comeback_date.isoformat()
-    if interests_collection.find_one(interest_dict):
-        return (None, 200)
-    else:
-        interests_collection.insert_one(interest_dict)
-        return (None, 200)
 
+    # Send message to Camunda
+    #camunda_client.correlate_message("interest", process_variables={"interest": json.dumps(interest_dict)})
+    camunda_message = {
+        "messageName": "interest",
+        "processVariables": {
+            "interest": {
+                "type": "String",
+                "value": json.dumps(interest_dict)},
+                "valueInfo": {
+                    "transient": True
+                }
+        }
+    }
+    r = requests.post(camunda_base_url+"/message", json=camunda_message)
+    return (None, r.status_code)
 
 def send_payment_information(payment_information=None):  # noqa: E501
     """sendPaymentInformation
