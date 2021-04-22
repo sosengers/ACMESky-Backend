4d38eaec8ff745cca21c35a09b8f543828716874
Marco Ferrati <marco.ferrati@gmail.com>
2021-04-22 18:54:26 +0200

    Add documentation

--- acmesky/camunda_connector/camunda_rest_client.py
+++ acmesky/camunda_connector/camunda_rest_client.py
@@ -1,9 +1,21 @@
 import requests
 from os import environ
 import logging
+
 BASE_URL: str = environ.get("CAMUNDA_BASE_URL", "http://camunda_acmesky:8080/engine-rest")
 
+
 def send_string_as_correlate_message(name: str, process_variables: list[tuple[str, str]], process_instance_id=None):
+    """
+    Send to Camunda Engine a correlate message with some process variables and an optional process_instance_id
+    @param name: the name of the message, used by the engine to know which Message Catch Event trigger
+    @param process_variables: the variables to pass to the process with the message
+    @param process_instance_id: the process instance id to use to correlate a message to a started process instance
+    @return: return the status code returned by Camunda
+    """
+
+    """ Generate a dictionary with the variables to send to Camunda 
+    """
     process_variables_dict = {}
     for variable in process_variables:
         variable_name = variable[0]
@@ -18,6 +30,8 @@ def send_string_as_correlate_message(name: str, process_variables: list[tuple[st
         "transient": True
     }
 
+    """ Create the message to send to Camunda starting from the parameters
+    """
     camunda_message = {
         "messageName": name,
         "processInstanceId": process_instance_id,
@@ -27,5 +41,4 @@ def send_string_as_correlate_message(name: str, process_variables: list[tuple[st
         "processVariables": process_variables_dict
     }
     logging.info(camunda_message)
-    return requests.post(BASE_URL+"/message", json=camunda_message)
-
+    return requests.post(BASE_URL + "/message", json=camunda_message)
--- acmesky/controllers/default_controller.py
+++ acmesky/controllers/default_controller.py
@@ -31,6 +31,10 @@ def buy_offer(offer_purchase_data=None):  # noqa: E501
     """
     if connexion.request.is_json:
         offer_purchase_data = OfferPurchaseData.from_dict(connexion.request.get_json())  # noqa: E501
+
+    """ Communication code generation
+        The communication code will be used to communicate with the WebSocket. It is some unique data hashed altogether.
+    """
     communication_code = str(hash((
         offer_purchase_data.offer_code,
         offer_purchase_data.name,
@@ -44,9 +48,13 @@ def buy_offer(offer_purchase_data=None):  # noqa: E501
     )
     ))
 
+    """ Sending as a correlate message to Camunda the data inserted by the user
+    """
     r = send_string_as_correlate_message("offer_purchase_data",
                                          [("offer_purchase_data", json.dumps(offer_purchase_data.to_dict()))])
-    # logging.error(f"DATA: {offer_purchase_data.to_dict()}")
+
+    """ Returning to the user the communication code to use with the WebSocket
+    """
     return BuyOfferResponse(communication_code=communication_code)
 
 
@@ -65,12 +73,16 @@ def publish_last_minute_offer(company_name, flight=None):  # noqa: E501
     if connexion.request.is_json:
         flights = [Flight.from_dict(d) for d in connexion.request.get_json()]  # noqa: E501
 
+    """ Convert the datetime to iso format
+        For each flights sent, the datetimes is converted in theit iso format 
+    """
     for flight in flights:
         flight.departure_datetime = flight.departure_datetime.isoformat()
         flight.arrival_datetime = flight.arrival_datetime.isoformat()
 
+    """ Generate the list of flights in their dict format to send to Camunda within a correlate message
+    """
     flights_dict = [f.to_dict() for f in flights]
-
     r = send_string_as_correlate_message("offers",
                                          [("offers", json.dumps(flights_dict)), ("company_name", str(company_name))])
     return None, r.status_code
@@ -89,11 +101,14 @@ def register_interest(interest=None):  # noqa: E501
     if connexion.request.is_json:
         interest = Interest.from_dict(connexion.request.get_json())  # noqa: E501
 
+    """ Convert the datetimes into their iso format
+    """
     interest_dict = interest.to_dict()
     interest_dict['min_departure_date'] = interest.min_departure_date.isoformat()
     interest_dict['max_comeback_date'] = interest.max_comeback_date.isoformat()
 
-    # Send message to Camunda
+    """ Send message with the interest to Camunda
+    """
     r = send_string_as_correlate_message("interest", [("interest", json.dumps(interest_dict))])
     if r.status_code >= 300:
         logging.error(f"Fail to send message to Camunda. Response: {r.text}")
@@ -113,10 +128,15 @@ def send_payment_information(payment_information=None):  # noqa: E501
     if connexion.request.is_json:
         payment_information = PaymentInformation.from_dict(connexion.request.get_json())  # noqa: E501
 
+    """ Open the connection to redis to get the right process instance id in the Camunda Engine (saved by a worker)
+    """
     redis_connection = Redis(host="acmesky_redis", port=6379, db=0)
     process_instance_id = redis_connection.get(payment_information.transaction_id).decode("utf-8")
     redis_connection.close()
 
+    """ Send a correlate message to Camunda with the information about the payment status. The message is for a specific
+        process instance, identified by the process_instance_id variable 
+    """
     r = send_string_as_correlate_message("payment_status",
                                          [("payment_status", json.dumps(payment_information.to_dict()))],
                                          process_instance_id)
